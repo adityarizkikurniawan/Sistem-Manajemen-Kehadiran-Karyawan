fc5a91538a621008d0ae56c2a067f1725feb734e
Aditya Rizki Kurniawan <adityarizkikurniawan95@gmail.com>
2026-04-05 11:17:17 +0700

    Menyelesaikan UI Dashboard dan Home

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // Halaman Daftar Kehadiran
 Route::get('/presensi', [PresensiController::class, 'index']);
+// Tambahkan ini di routes/web.php
+Route::view('/home', 'home');
+Route::view('/dashboard', 'dashboard');
+Route::view('/product', 'product');
