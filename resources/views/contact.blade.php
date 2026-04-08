ef0b5431a7b540cf771655c2677c73f62a957605
Sistem manajemen kehadiran karyawan <muhammadhoirulfarhan@gmail.com>
2026-04-08 20:37:35 +0700

    Halaman Contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,36 @@
-<div>
-    <div>
-    <h1>Halaman Kontak</h1>
-    <p>Silahkan Hubungi admin di: admin@perusahaan.com</p>
-</div>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+</head>
+
+<body>
+    <nav>
+    <a href="/about">About</a> |
+    <a href="/contact">Contact</a>
+</nav>
+<hr>
+
+    <h1>Contact Us</h1>
+
+    <p>Silakan hubungi kami melalui:</p>
+
+    <ul>
+        <li>Email: example@gmail.com</li>
+        <li>Phone: 0812-3456-7890</li>
+    </ul>
+
+    <form>
+        <label>Nama:</label><br>
+        <input type="text" name="nama"><br><br>
+
+        <label>Pesan:</label><br>
+        <textarea name="pesan"></textarea><br><br>
+
+        <button type="submit">Kirim</button>
+    </form>
+
+    <a href="/about">Kembali ke About</a>
+
+</body>
+</html> 
