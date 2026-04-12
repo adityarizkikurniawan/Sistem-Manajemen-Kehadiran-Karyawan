d0baeeb10c166134096f5cb54263bcc257be31bc
Aditya Rizki Kurniawan <adityarizkikurniawan95@gmail.com>
2026-04-12 21:14:36 +0700

    aku nyari UI yang bagus dlu ini masih jelek

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 // Halaman Daftar Kehadiran
 Route::get('/presensi', [PresensiController::class, 'index']);
-// Tambahkan ini di routes/web.php
+
 Route::view('/home', 'home');
 Route::view('/dashboard', 'dashboard');
 Route::view('/product', 'product');
+Route::get('/app', function () 
+{
+    return view('app');
+});
